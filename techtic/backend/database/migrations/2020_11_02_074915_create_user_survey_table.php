529737e76ebf69d027efb6bfd1efbf8dab47e908
neel patel <neel.radicalloop@gmail.com>
2020-11-02 18:34:50 +0530

    user_id unique key

--- techtic/backend/database/migrations/2020_11_02_074915_create_user_survey_table.php
+++ techtic/backend/database/migrations/2020_11_02_074915_create_user_survey_table.php
@@ -16,7 +16,7 @@ class CreateUserSurveyTable extends Migration
         Schema::create('user_surveys', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onUpdate('RESTRICT')->onDelete('CASCADE');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('RESTRICT')->onDelete('CASCADE')->unique();
             $table->string('university_name');
             $table->string('worth_money', 3);
             $table->string('seminar_rate', 5);
